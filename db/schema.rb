55131a3841bc5a3a47f8166faa87e6aecf01d341
Dawa Ometto <d.ometto@gmail.com>
2013-07-20 21:13:49 +0200

    Removed ActiveAdmin from DB schema. Removed deprecated devise calls.

--- db/schema.rb
+++ db/schema.rb
@@ -11,27 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120812110323) do
-
-  create_table "active_admin_comments", :force => true do |t|
-    t.string   "resource_id",   :null => false
-    t.string   "resource_type", :null => false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.text     "body"
-    t.datetime "created_at",    :null => false
-    t.datetime "updated_at",    :null => false
-    t.string   "namespace"
-  end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
+ActiveRecord::Schema.define(:version => 20130604191727) do
 
   create_table "repositories", :force => true do |t|
     t.string   "name",       :default => ""
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
+    t.boolean  "public",     :default => false
   end
 
   create_table "roles", :force => true do |t|
@@ -43,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20120812110323) do
   end
 
   create_table "users", :force => true do |t|
+    t.string   "username",               :default => "", :null => false
     t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "name",                   :default => ""
@@ -62,5 +49,6 @@ ActiveRecord::Schema.define(:version => 20120812110323) do
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
