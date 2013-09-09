3c958d85552e76fbd1403153649c48b0e562736b
Dawa Ometto <dawa.ometto@phil.uu.nl>
2013-09-09 12:43:06 +0200

    Don't seed test objects.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,5 @@
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
 
-user = User.create!(:username => "testuser", :name => "Test User", :email => "test@repotag.org", :password => "koekje123")
-repo = Repository.create!(:name => "Test Repo")
-role = Role.create(:title => :owner)
-role.repository_id = repo.id
-role.user_id = user.id
-role.save
-
-user = User.create(:username => "admin_user", :name => "Admin User", :email => "repotag@repotag.org", :password => "koekje123")
-user.admin = true
-user.save
-repo = Repository.create!(:name => "Super Secret Repo")
-role = Role.create(:title => :owner)
-role.repository_id = repo.id
-role.user_id = user.id
-role.save
-
 smtp_settings = Setting.create(:name => :smtp_settings, :settings => {:address => 'localhost', :port => 25})
 smtp_settings.save
