d8ad9baa36173d87f37a78053c2e82bb3ed883d8
Dawa Ometto <dawa.ometto@phil.uu.nl>
2013-09-09 13:28:32 +0200

    Smaller x-editable buttons, and smaller settings form table (auto size).

--- app/assets/javascripts/xeditable.js
+++ app/assets/javascripts/xeditable.js
@@ -1,2 +1,5 @@
 //= require bootstrap-editable
 //= require admin/xeditable
+
+$.fn.editableform.buttons = '<button type="submit" class="btn btn-primary btn-xs editable-submit"><i class="icon-ok icon-white"></i></button>
+<button type="button" class="btn btn btn-xs editable-cancel"><i class="icon-remove"></i></button>'
