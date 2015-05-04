8d31bcafa3e6581922b5406df284753667b4a7cb
Dawa Ometto <dawa.ometto@phil.uu.nl>
2015-05-04 22:14:15 +0200

    Fix all specs.

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,8 @@
 require 'rubygems'
 
+# See https://github.com/jruby/jruby/wiki/UnlimitedStrengthCrypto
+java.lang.Class.for_name('javax.crypto.JceSecurity').get_declared_field('isRestricted').tap{|f| f.accessible = true; f.set nil, false}
+
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Repotag::Application.config.secret_token = '45735229d13da6d6cbb1919aa8c7fa44333024c69d0fac3131703093b326841327f6559bdfcdac66aa9fc7881a5aff570b50812efc4842833fb7e9513ab11d40'
