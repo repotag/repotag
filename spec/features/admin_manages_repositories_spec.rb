2610fb1d30e1ba33cd6b9908ef5e7a06638ecc96
Dawa Ometto <dawa.ometto@phil.uu.nl>
2015-08-23 01:47:35 +0200

    Fix selenium tests

--- spec/features/admin_manages_repositories_spec.rb
+++ spec/features/admin_manages_repositories_spec.rb
@@ -1,16 +1,27 @@
 require 'spec_helper'
 
-feature "Admin manages repositories" do
-  
-  before(:each) do
-    # Create admin user to run the tests with
+def login_helper
+  Proc.new {
     @admin = FactoryGirl.create(:user)
     @admin.set_admin(true)
     
     @repos = []
     2.times { @repos << FactoryGirl.create(:repository) }
     
-    http_auth(@admin.username,'koekje123')
+    http_auth(@admin.username, @admin.password)
+  }
+end
+
+def create_repository(name)
+  visit "/admin/repositories"
+  click_link 'New Repository'
+  fill_in "Name", with: name
+  click_button 'Save Repository'
+end
+
+feature "Admin manages repositories" do
+  before :each do
+    login_helper.call
   end
   
   scenario "by viewing all repositories" do
@@ -24,22 +35,20 @@ feature "Admin manages repositories" do
     create_repository("Turtles")
     expect(page).to have_text "Repository was successfully created."
   end
-  
-  # scenario "by deleting a repository", :js => true do
-  #   skip "Javascript not working since upgrade to Rails 4.2."
-  #   create_repository("Turtles")
-  #   click_button "delete"
-  #   page.accept_alert 'Are you sure?' do
-  #       click_button('Yes')
-  #   end
-  #   expect(page).to have_text "Repository Turtles was successfully deleted."
-  # end
-  
-  def create_repository(name)
-    visit "admin/repositories"
-    click_link 'New Repository'
-    fill_in "Name", with: "Turtles"
-    click_button 'Save Repository'
+end
+
+feature "Admin manages repositories with js", :js => true do
+  before :each do
+    login_helper.call
   end
   
+  scenario "by deleting a repository" do
+    name = "Turtles"
+    create_repository(name)
+    visit "/admin/repositories"
+    page.accept_alert 'Are you sure?' do
+      all(".fa-trash-o").last.click
+    end
+    expect(page).to have_text "Repository #{name} was successfully deleted."
+  end
 end
