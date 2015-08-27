ac195d0ffea2094dd2216cbad71744f891e18f43
Bart Kamphorst <bart@kamphorst.com>
2015-08-28 00:10:12 +0200

    cleanup of javascript assets, started move towards Opal.

---
+++ app/assets/javascripts/repositories.js.rb
@@ -0,0 +1,8 @@
+# Place all the behaviors and hooks related to the matching controller here.
+# All this logic will automatically be available in application.js.
+# You can use Opal in this file.
+
+Document.ready? do
+  # client = this.ZeroClipboard.new(Element.find('#d_clip_button') )
+	`client = new ZeroClipboard(document.getElementById('d_clip_button') )`
+end
