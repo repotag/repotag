a239da001a100c4265c1823a0e5e9564f136e0d4
Bart Kamphorst <bart@kamphorst.com>
2015-09-11 22:28:08 +0200

    Made admin users table into a fancy datatable.

---
+++ app/assets/javascripts/admin/admin-user-table.js
@@ -0,0 +1,6 @@
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+
+$( document ).ready(function() {
+	var userTable = $("#admin-users-table").DataTable();
+});
