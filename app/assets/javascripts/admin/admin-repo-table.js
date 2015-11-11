d5258d878ba9ddc72d5e5d56792de2aa9ac08d13
Bart Kamphorst <bart@kamphorst.com>
2015-11-11 19:55:44 +0100

    Simplify require statements.

--- app/assets/javascripts/admin/admin-repo-table.js
+++ app/assets/javascripts/admin/admin-repo-table.js
@@ -1,5 +1,3 @@
-//= require dataTables/jquery.dataTables
-//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 
 $( document ).ready(function() {
 	var repoTable = $("#admin-repositories-table").DataTable();
--- app/assets/javascripts/admin/admin-user-table.js
+++ app/assets/javascripts/admin/admin-user-table.js
@@ -1,5 +1,3 @@
-//= require dataTables/jquery.dataTables
-//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 
 $( document ).ready(function() {
 	var userTable = $("#admin-users-table").DataTable();
