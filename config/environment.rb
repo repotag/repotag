02098ea28bba90d68bc39ba37c2d1f6f73bcdef9
Bart Kamphorst <bart@kamphorst.com>
2015-11-13 00:31:33 +0100

    Read production database info and secret from external config file.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+config_location = java.lang.System.getProperty('config') || ::File.join(Dir.pwd, 'repotag.yml')
+
+REPOTAG_CONFIG = YAML.load_file(config_location)
+
 # Initialize the rails application
 Repotag::Application.initialize!
