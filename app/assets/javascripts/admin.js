a3571276aa6e95ab64236c1ae846814bf694935f
Bart Kamphorst <bart@kamphorst.com>
2015-11-13 14:16:29 +0100

    Do not require dataTables until a new version of jquery-datatables-rails has been released.

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -1,6 +1,2 @@
 // Admin
 //= require admin/xeditable
-//= require dataTables/jquery.dataTables
-//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
-//= require admin/admin-repo-table
-//= require admin/admin-user-table
