07247b2a33924953ea5efcc046e4e92ef4ec9a22
Bart Kamphorst <bart@kamphorst.com>
2015-11-13 14:12:49 +0100

    Minor fix for finding repotag config file in working directory in production.

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config_location = java.lang.System.getProperty('config') || ::File.join(Dir.pwd, 'repotag.yml')
+config_location = java.lang.System.getProperty('config') || ::File.join(java.lang.System.getProperty("user.dir"), 'repotag.yml')
 
 REPOTAG_CONFIG = YAML.load_file(config_location)
 
 # Initialize the rails application
 Repotag::Application.initialize!
+
