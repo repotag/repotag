7af7fc66894dfd964ab226d740a80bd755457649
Bart Kamphorst <bart@kamphorst.com>
2015-11-21 17:51:56 +0100

    Mailers now actually use custom smtp settings.

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -1,15 +1,15 @@
 class Mailer < ActionMailer::Base
-  default :from => "no-reply@repotag.org"
-  default :to => "repotag@repotag.org"
+  default from: "no-reply@repotag.org"
+  default to: "repotag@repotag.org"
 
   def new_message(message)
    @message = message
-   mail(:subject => "[Repotag] #{@message}")
+   mail(subject: "[Repotag] #{@message}", delivery_method_options: Setting.get(:smtp_settings).settings )
   end
 
   def test_email(user)
     @user = user
-    mail(:subject => "[Repotag] Auto-generated test message.", :to => user.email)
+    mail(subject: "[Repotag] Auto-generated test message.", to: user.email, delivery_method_options: Setting.get(:smtp_settings).settings )
   end
 
 end
